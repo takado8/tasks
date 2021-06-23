0552f599a0fee88205cecf1fba775a6f10c2d6b4
takado <kornelgut@gmail.com>
2021-06-23 20:02:35 +0200

    update showtasks.bat

--- showtasks.bat
+++ showtasks.bat
@@ -1,7 +1,11 @@
 call runcrud
 if "%ERRORLEVEL%" == "0" goto start_browser
 echo errors occurred.
+goto end
 
 :start_browser
 start "C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:8080/crud/v1/task/getTasks
 echo Success.
+
+:end
+echo end of program
