11d92104b5d3fbed8a9e381b824b23a96045cd90
takado <kornelgut@gmail.com>
2021-06-29 21:14:13 +0200

    add CrossOrigin to TaskController

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -9,7 +9,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+
 @RestController
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @RequestMapping("/v1/task")
 public class TaskController {
