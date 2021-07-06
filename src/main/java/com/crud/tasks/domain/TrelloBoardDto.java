8a8eb34703336c464067402872f95693007c9f38
takado <kornelgut@gmail.com>
2021-07-06 02:51:22 +0200

    update

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -1,9 +1,23 @@
 package com.crud.tasks.domain;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloBoardDto {
-    private String name;
+
+    @JsonProperty("id")
     private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("lists")
+    private List<TrelloListDto> lists;
+
 }
