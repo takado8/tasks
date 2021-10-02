43980870967614ccca55f36708eb956d0b5301d6
takado <kornelgut@gmail.com>
2021-10-02 21:55:36 +0200

    32.2 mail templates

--- src/main/java/com/crud/tasks/trello/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/trello/domain/TrelloBoardDto.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloBoardDto {
 
--- src/main/java/com/crud/tasks/trello/domain/TrelloListDto.java
+++ src/main/java/com/crud/tasks/trello/domain/TrelloListDto.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
 @AllArgsConstructor
 public class TrelloListDto {
     @JsonProperty("id")
